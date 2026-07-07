ea433d3852dcb0bdf007c89846c5caae80b89299
chegeni-eng <eqorbani@gmail.com>
2026-07-07 16:40:42 +0200

    Refactor app.js for prediction submission logic

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Prediction deadline
-const DEADLINE = new Date("2026-07-08T23:59:00");
+const DEADLINE = new Date("2026-07-09T20:00:00");
 
 const teams = [
 
